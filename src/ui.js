47e5ba0aa6c6156e83223e84096ef9e76b51f248
Johan Adami <johan.adami@gmail.com>
2026-02-28 02:31:52 -0500

    fix alignment

--- src/ui.js
+++ src/ui.js
@@ -35,6 +35,8 @@ export function enforceNonNegativeIntegerInput(input) {
       "Delete",
       "ArrowLeft",
       "ArrowRight",
+      "ArrowUp",
+      "ArrowDown",
       "Home",
       "End",
       "Tab",
