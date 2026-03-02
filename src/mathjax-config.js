4d635e576c1656f18b534e007b972465b53f51af
Johan Adami <johan.adami@gmail.com>
2026-03-01 21:48:31 -0500

    fix mathjax errors

---
+++ src/mathjax-config.js
@@ -0,0 +1,13 @@
+window.MathJax = {
+  startup: {
+    typeset: false,
+  },
+  chtml: {
+    displayAlign: "left",
+    displayIndent: "0",
+    linebreaks: {
+      automatic: true,
+      width: "container",
+    },
+  },
+};
